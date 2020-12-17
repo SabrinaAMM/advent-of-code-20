d18ad684a3122c055e7d6056c815f9a6d5ec01ce
Sabrina Ammerl <michaela.ammerl@gmx.de>
2020-12-17 19:20:03 +0100

    solved day one

--- day1/day_one.rb
+++ day1/day_one.rb
@@ -1,4 +1,4 @@
-# find the two keys from the given data whose sum is 2020
+# Part 1: find the two keys from the given data whose sum is 2020
 
 require 'csv'
 
@@ -8,7 +8,7 @@ CSV.foreach('data.csv') do |row|
   keys << row[0].to_i
 end
 
-def find_keys(array)
+def find_two_keys(array)
   filtered = []
   array.each do |element|
     i = 1
@@ -21,4 +21,26 @@ def find_keys(array)
   filtered
 end
 
-p find_keys(keys)
+p find_two_keys(keys)
+
+# Part 2: find the three keys from the given data whose sum is 2020
+
+def find_three_keys(array)
+  filtered = []
+
+  array.each do |element|
+    i = 1
+    while i < array.length
+      j = i
+      while j < array.length
+        sum = element + array[i] + array[j]
+        j += 1
+        filtered << element if sum == 2020
+      end
+      i += 1
+    end
+  end
+  filtered
+end
+
+p find_three_keys(keys)
