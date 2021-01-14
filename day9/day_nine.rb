7d84fa0d1e30466db43a5c23cd7a379a6fba1ca2
Sabrina Ammerl <michaela.ammerl@gmx.de>
2021-01-14 15:39:06 +0100

    day 9 solved

--- day9/day_nine.rb
+++ day9/day_nine.rb
@@ -1,19 +1,42 @@
 numbers = File.readlines('input_day_9.txt').map do |line|
-  line = line.gsub(/\n/, '').to_i
+  line.gsub(/\n/, '').to_i
 end
+
+# part one
 
 index_start = 0
 index_end = 24
 index_tested_number = 25
+invalid_number = 0
 
 loop do
   sum_of_number_pairs = numbers[index_start..index_end].combination(2).to_a.map { |array| array.inject(0, :+) }
 
   unless sum_of_number_pairs.include?(numbers[index_tested_number])
-    pp numbers[index_tested_number]
+    invalid_number = numbers[index_tested_number]
+    p "The invalid number is #{invalid_number}"
+
     break
   end
   index_start += 1
   index_end += 1
   index_tested_number += 1
+end
+
+# part two
+
+numbers.each_with_index do |_number, index|
+  sum = 0
+  i = index + 1
+  while i < numbers.size && sum <= invalid_number
+    sum = numbers[index..i].sum
+
+    if sum == invalid_number
+      encryption_weakness = numbers[index..i].minmax.sum
+      p "The encryption weakness is #{encryption_weakness}"
+      break
+
+    end
+    i += 1
+  end
 end
